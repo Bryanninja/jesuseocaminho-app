24bfe83229b3c5f02eafcad649c4eac9e1b2f0d3
Álex Bryan <bryannascimentopl800@gmail.com>
2026-03-07 11:20:06 -0300

    chore: add plan colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontFamily:{
+      colors: {
+        plan: {
+          'light-black': '#2B2B2B',
+          'green-brand': '#69BB8A',
+          'green-primary': '#68BA6D',
+          'green-yellow': '#84BA68',
+          'green-opaque': '#A6BAAE',
+          'blue-green': '#68BAA5',
+          'blue-light-green': '#68B4BA',
+          'black-text': '#3F3F3F',
+        },
+      },
+      backgroundImage: {
+        'hero-gradient':
+          'linear-gradient(to bottom, #68BA6D, #69BB8A, #84BA68, #68BAA5)',
+      },
+
+      fontFamily: {
         sans: ['Roboto', 'sans-serif'],
-        raleway: ['Raleway', 'sans-serif']
+        raleway: ['Raleway', 'sans-serif'],
       },
     },
   },
   plugins: [],
-}
+};
