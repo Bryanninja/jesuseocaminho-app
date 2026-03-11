bb01b3520001f130cf31363ec918ca44cf72b2a8
Álex Bryan <bryannascimentopl800@gmail.com>
2026-03-11 10:46:37 -0300

    feat: ensure Reading component can receive props

--- src/components/Reading.jsx
+++ src/components/Reading.jsx
@@ -4,15 +4,15 @@ import Bible from '../assets/SVG/Bible.svg';
 import Button from './Button';
 import Checkbox from './Checkbox';
 
-const Reading = () => {
+const Reading = ({ data, chapters }) => {
   const [marcado, setMarcado] = useState(false);
   return (
     <div className="flex items-center justify-between rounded-3xl bg-plan-black-text p-4">
       <div className="flex items-center gap-4 md:gap-6">
         <Checkbox isChecked={marcado} onClick={() => setMarcado(!marcado)} />
         <div className="flex flex-col">
-          <h3 className="text-2xl font-bold text-white">24/Mar</h3>
-          <p className="text-lg text-white">Gn 45-47, Mt 16</p>
+          <h3 className="text-2xl font-bold text-white">{data}</h3>
+          <p className="text-lg text-white">{chapters}</p>
         </div>
       </div>
 
