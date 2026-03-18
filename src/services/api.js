6cdcbf4cb0f4760f8a4eb5d7dd4282c2384575ff
Álex Bryan <bryannascimentopl800@gmail.com>
2026-03-18 11:31:22 -0300

    chore: add authorization bearer token

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,15 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://www.abibliadigital.com.br/api',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdHIiOiJUdWUgTWFyIDE3IDIwMjYgMTM6NDI6MDkgR01UKzAwMDAuYnJ5YW5uY29kZUBnbWFpbC5jb20iLCJpYXQiOjE3NzM3NTQ5Mjl9.VGv_RAobAvtMagTNEV_dU8ak0BDP857TWKJs9FsDhwM',
+  },
 });
+
+export default api;
 
 /**
  * Função para buscar o conteúdo de um único capítulo
